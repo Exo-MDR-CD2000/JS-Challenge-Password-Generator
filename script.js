1985660519f3c7f34c9246135e36d7e803e20d60
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-06-30 18:27:41 -0700

    commented out original code on bottom rows

--- script.js
+++ script.js
@@ -98,19 +98,19 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-//below is the original code just for clarity sake
+// //below is the original code just for clarity sake
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
